6b85c5a3b8b039a1403399cf9b5da8f2a269bdb1
Thais <thais.scott@richmond.edu>
2022-07-26 10:20:51 -0500

    Doc String

--- msf_2022b/functions.py
+++ msf_2022b/functions.py
@@ -1,5 +1,6 @@
 """Provide the primary functions."""
 
+from typing import Any
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,7 +9,28 @@ from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
 # matplotlib notebook
 
 
-def calculate_distance(rA, rB):
+def calculate_distance(rA: np.ndarray, rB: np.ndarray)-> np.floating:
+    """
+    Calculate the distance between two points
+
+    Parameters
+    __________
+
+    rA, rB : np.ndarray
+        The coordinates of each point. 
+
+    Returns 
+    _______
+    dist: np.floating 
+         The distance between the two points.
+
+    Examples
+    ________
+    >>> r1 = np.array([0,0,0])
+    >>> r1 = np.array([0,1.0,0])
+    >>> calculate_distance(r1,r2)
+    1.0
+    """ 
     # This function calculates the distance between two points given as numpy arrays.
     d = rA - rB
     dist = np.linalg.norm(d)
@@ -178,3 +200,4 @@ atom_colors = {
     "Br": "#F4A460",
     "S": "yellow",
 }
+
